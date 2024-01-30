33c11f84cbdff4e0fceb94b51bfaec9e51ba6906
Eunryeong Kim <ker26772@gmail.com>
2024-01-31 07:07:08 +0900

    0131

---
+++ script.js
@@ -0,0 +1,15 @@
+// function updateCheckedState() {
+//   let radioBtn = document.getElementById('tab01');
+  
+//   if (window.innerWidth >= 768) {
+//     // 창 너비가 768px 이상인 경우, checked 속성 제거
+//     radioBtn.removeAttribute('checked');
+//   } else {
+//     // 창 너비가 768px 미만인 경우, checked 속성 추가
+//     radioBtn.setAttribute('checked', 'checked');
+//   }
+// }
+// // 페이지 로딩 시 초기 확인
+// updateCheckedState();
+// // 창 크기 변경 이벤트에 대한 추가
+// window.addEventListener('resize', updateCheckedState);
