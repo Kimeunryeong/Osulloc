3dad34f320440f1b3617079cd16a5dd25e829048
Eunryeong Kim <ker26772@gmail.com>
2024-02-01 01:02:38 +0900

    0201

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
 // 오설록 인트로 슬라이드 함수
-// let introEff = document.querySelector(".web-textBox");
+let introEff = document.querySelector(".web-textBox");
 
-// window.addEventListener("scroll", function () {
-//   let value = window.scrollY;
-//   console.log("scrollY", value);
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  console.log("scrollY", value);
 
-//   if (value >= 300 && value <= 1100) {
-//     introEff.style.animation = "slide 2s ease-in-out forwards";
-//   } else if (value < 299 || value > 1111) {
-//     introEff.style.animation = "disappear 2s ease-in-out forwards";
-//   }
-// });
+  if (value >= 300 && value <= 1100) {
+    introEff.style.animation = "slide 1s ease-in-out forwards";
+  } else if (value < 299 || value > 1111) {
+    introEff.style.animation = "disappear 1s ease-in-out forwards";
+  }
+});
 
 // // 티하우스 오퍼시티 함수
 // let textBox2 = document.querySelector(".web-textBox2");
