65ebb69585fecc663e7070663347068fb278b108
erkim <ker26772@gmail.com>
2024-02-06 19:51:36 +0900

    20240206

--- script.js
+++ script.js
@@ -1,67 +1,19 @@
-// 오설록 인트로 슬라이드 함수
-let introEff = document.querySelector(".web-textBox");
-
-window.addEventListener("scroll", function () {
-  let value = window.scrollY;
-  console.log("scrollY", value);
-    // value <668 0
-    // value > 668 1 
-    
-  if (value >= 300 && value <= 1300) {
-    introEff.style.animation = "slide 1s ease-in-out forwards";
-  } else if (value < 299 || value > 1400) {
-    introEff.style.animation = "disappear 1s ease-in-out forwards";
-  }
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((box) => {
+    if (box.isIntersecting) {
+      box.target.style.opacity = 1;
+    } else {
+      box.target.style.opacity = 0;
+    }
+  });
 });
 
 
+let introBoxes = document.querySelectorAll('.introBox');
 
+observer.observe(introBoxes[0]);
+observer.observe(introBoxes[1]);
+observer.observe(introBoxes[2]);
+observer.observe(introBoxes[3]);
+observer.observe(introBoxes[4]);
 
-
-
-// // 데스크톱 환경에서 실행될 이벤트 리스너 함수
-// function desktopScrollHandler() {
-//   console.log('데스크톱 스크롤 위치:', window.scrollY);
-//   // 데스크톱 환경에 맞는 스크롤 효과 코드 추가
-// }
-
-// // 모바일 환경에서 실행될 이벤트 리스너 함수
-// function mobileScrollHandler() {
-//   console.log('모바일 스크롤 위치:', window.scrollY);
-//   // 모바일 환경에 맞는 스크롤 효과 코드 추가
-// }
-
-// // 뷰포트 너비에 따라 적절한 이벤트 리스너 함수를 할당
-// if (window.innerWidth >= 768) {
-//   window.addEventListener('scroll', desktopScrollHandler);
-// } else {
-//   window.addEventListener('scroll', mobileScrollHandler);
-// }
-
-// // 티하우스 오퍼시티 함수
-// let textBox2 = document.querySelector(".web-textBox2");
-
-// window.addEventListener("scroll", function () {
-//   let scrollPosition = window.scrollY;
-//   console.log("scrollY", scrollPosition);
-
-//   if (scrollPosition >= 1730 && scrollPosition < 2650) {
-//     textBox2.style.opacity = 1;
-//   } else {
-//     textBox2.style.opacity = 0;
-//   }
-// });
-
-
-// 인조이 오설록
-// let enjoySection = document.getElementById("enjoy");
-
-// window.addEventListener("scroll", function () {
-//   let scrollPosition1 = window.scrollY;
-
-//   if (scrollPosition1 < 6500) {
-//     enjoySection.style.opacity = 0;
-//   } else {
-//     enjoySection.style.opacity = 1;
-//   }
-// });
