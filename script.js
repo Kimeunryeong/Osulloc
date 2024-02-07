98bfd0d1595c52697e82e6e179684c6c86d5549d
Eunryeong Kim <ker26772@gmail.com>
2024-02-08 03:35:46 +0900

    20240208

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 
 let observer = new IntersectionObserver((entries) => {
   entries.forEach((box) => {
     if (box.isIntersecting) {
@@ -8,12 +9,48 @@ let observer = new IntersectionObserver((entries) => {
   });
 });
 
+let introBoxes = document.querySelectorAll(".introBox");
+for (let i = 0; i < 4; i++) {
+  observer.observe(introBoxes[i]);
+}
 
-let introBoxes = document.querySelectorAll('.introBox');
+let newObserver = new IntersectionObserver((entries) => {
+  entries.forEach((box) => {
+    if (box.isIntersecting) {
+      box.target.style.opacity = 1;
+      box.target.style.transform = "translateX(0)";
+    } else {
+      box.target.style.opacity = 0;
+      box.target.style.transform = "translateX(-200px)";
+    }
+  });
+});
 
-observer.observe(introBoxes[0]);
-observer.observe(introBoxes[1]);
-observer.observe(introBoxes[2]);
-observer.observe(introBoxes[3]);
-observer.observe(introBoxes[4]);
+// 새로운 observer 적용
+newObserver.observe(introBoxes[4]);
 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll("#web-main div");
+  let currentIndex = 0;
+
+  function showImage(index) {
+    images.forEach((image, i) => {
+      if (i === index) {
+        image.classList.add("active");
+      } else {
+        image.classList.remove("active");
+      }
+    });
+  }
+
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  }
+
+  // 첫 번째 이미지 표시
+  showImage(currentIndex);
+  setInterval(nextImage, 4000); 
+});
